32c9f094a9373609916a7bde55114fb98a4dde50
Július Retzer <jul.retzer@gmail.com>
2025-03-26 15:49:13 +0100

    New beggining

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-import App from "next/app";
-import Router from "next/router";
-
-import * as gtag from "../lib/gtag";
-
-Router.events.on("routeChangeComplete", url => gtag.pageview(url));
-
-export default App;
